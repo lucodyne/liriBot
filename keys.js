0b640c5945ef7f86e639539206881925f3ae9c1c
Sean Lim <lim_s@outlook.com>
2019-10-05 14:23:30 -0500

    added key for seatgeek

--- keys.js
+++ keys.js
@@ -1,9 +1,14 @@
-require("dotenv").config()
+require("dotenv").config();
 exports.spotifyKey = {
   id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET,
+  secret: process.env.SPOTIFY_SECRET
 };
-console.log(process.env.SPOTIFY_ID)
+
 exports.omdbKey = {
   key: process.env.OMDB_KEY
+};
+
+exports.seatGeekKey = {
+  client_id: process.env.SEATGEEK_ID,
+  secret: process.env.SEATGEEK_SECRET
 };
